47ea99ac59556a65e57cecf4a202c4180cddefe4
emerson <emerson.jara1998@gmail.com>
2021-09-21 22:25:57 -0500

    first commit

---
+++ Assets/_DrugVR/Scripts/AppController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppController : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject bannerPanel;
+    public GameObject doorsPanel;
+    public GameObject doorModels;
+
+    [Header("Buttons")]
+    public Button initBtn;
+    public Button exitBtn;
+    public Button continueBtn;
+
+    [Header("App Data")]
+    public AudioClip openDoorSound;
+    public List<AppData> appDatas;
+
+    private AppData currentData;
+    private GameObject currentScene;
+
+    private void Start()
+    {
+        initBtn.onClick.AddListener(InitOnClick);
+        exitBtn.onClick.AddListener(ExitOnClick);
+        continueBtn.onClick.AddListener(EnableDoorPanel);
+
+        //set event to btns
+        foreach (var item in appDatas)
+        {
+            item.doorBtn.onClick.AddListener(() => 
+            {
+                item.doorAnim.SetTrigger("open");
+                item.opened = true;
+                item.doorBtn.gameObject.SetActive(false);
+                GetComponent<AudioSource>().PlayOneShot(openDoorSound);
+                EnableDoorsBtn(false);
+                currentData = item;
+                StartCoroutine(LoadScene());
+            });
+        }
+    }
+
+    private void InitOnClick()
+    {
+        mainPanel.SetActive(false);
+        bannerPanel.SetActive(true);
+    }
+
+    private void ExitOnClick()
+    {
+        Application.Quit();
+    }
+
+    private void EnableDoorPanel()
+    {
+        bannerPanel.SetActive(false);
+        doorsPanel.SetActive(true);
+        doorModels.SetActive(true);
+    }
+
+    private void EnableDoorsBtn(bool value)
+    {
+        if (value == false)
+        {
+            foreach (var item in appDatas)
+            {
+                item.doorBtn.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var item in appDatas)
+            {
+                if(item.opened == false)
+                item.doorBtn.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2f);
+        currentScene = Instantiate(currentData.scene);
+        doorModels.SetActive(false);
+        doorsPanel.SetActive(false);
+    }
+}
+
+[System.Serializable]
+public class AppData
+{
+    public Button doorBtn;
+    public Animator doorAnim;
+    public GameObject scene;
+    public bool opened;
+}
---
+++ Assets/_DrugVR/Shaders/Scripts/ImageWithRoundedCorners.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageWithRoundedCorners : MonoBehaviour {
+	private static readonly int Props = Shader.PropertyToID("_WidthHeightRadius");
+
+	public float radius;
+
+	void OnRectTransformDimensionsChange(){
+		Refresh();
+	}
+	
+	private void OnValidate(){
+		Refresh();
+	}
+
+	private void Refresh(){
+
+		if (GetComponent<Image>() != null)
+		{
+			var rect = ((RectTransform)transform).rect;
+			GetComponent<Image>().material = new Material(Shader.Find("UI/RoundedCorners/RoundedCorners"));
+			GetComponent<Image>().material.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
+		}
+
+		if (GetComponent<RawImage>() != null)
+		{
+			var rect = ((RectTransform)transform).rect;
+			GetComponent<RawImage>().material = new Material(Shader.Find("UI/RoundedCorners/RoundedCorners"));
+			GetComponent<RawImage>().material.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
+		}
+	}
+}
